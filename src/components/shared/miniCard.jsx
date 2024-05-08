6a0d424926186e1d59b5d2f72c9869a303cd1813
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-05-08 20:02:16 +0530

    related card edit

--- src/components/shared/miniCard.jsx
+++ src/components/shared/miniCard.jsx
@@ -6,11 +6,11 @@ const MiniCard = () => {
   const truncText = getTruncatedText(string, 48);
   return (
     <a href="/">
-      <div className="xl:max-w-[20vw] post-border border-0 lg:border group/mini">
+      <div className="xl:max-w-[20vw] post-border border-0 lg:border group/mini overflow-hidden">
         <img
           src="https://images.unsplash.com/photo-1653103674098-6ed995323607"
           alt="mini-card"
-          className="w-full h-72 object-cover object-center"
+          className="w-full h-72 object-cover object-center group-hover/mini:scale-105 transition-transform duration-200"
         />
         <div className="p-8 group-hover/mini:text-purple-600 transition-colors duration-200">
           <h5>{truncText}</h5>
