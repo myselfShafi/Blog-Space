dd0e7d7f573d5384a1d2185cc584251200b729fb
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-05-28 19:53:54 +0530

    Update lazyImage.jsx

--- src/components/shared/lazyImage.jsx
+++ src/components/shared/lazyImage.jsx
@@ -1,14 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ImageLoader from "../loaders/imgLoader";
+
+const options = {
+  rootMargin: "0px 0px 300px",
+};
 
 const LazyImage = ({
   wrapperClass,
   loaderClass,
   loaderHeight,
+  src,
   className,
   ...props
 }) => {
   const [loading, setLoading] = useState(true);
+  const [url, setUrl] = useState("");
+  let ImageRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries, self) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setUrl(src);
+          self.unobserve(entry.target);
+          console.log("is intersecting...");
+        }
+      });
+    }, options);
+
+    observer.observe(ImageRef.current);
+
+    return () => {
+      if (ImageRef && ImageRef.current) {
+        observer.unobserve(ImageRef.current);
+      }
+    };
+  }, [src]);
 
   return (
     <div className={`relative ${wrapperClass}`}>
@@ -21,6 +48,8 @@ const LazyImage = ({
         onLoad={() => {
           setLoading(false);
         }}
+        ref={ImageRef}
+        src={url}
         className={`transition-opacity duration-1000 ${
           loading ? "h-0 opacity-0" : `${className} opacity-100`
         }`}
