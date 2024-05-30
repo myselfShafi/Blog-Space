af5abde531277d2954be56e6882cfd21d34e4d29
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-05-30 22:09:05 +0530

    Update manageArticleCount.js

--- functions/manageArticleCount.js
+++ functions/manageArticleCount.js
@@ -1,21 +1,3 @@
-import { Client, Databases } from "node-appwrite";
-import { envConfig } from "../src/config";
-
 export default async ({ req, res, log, error }) => {
-  const client = new Client();
-  const databases = new Databases(client);
-
-  client
-    .setEndpoint(envConfig.appWriteURL)
-    .setProject(envConfig.appWriteProjectId);
-
-  const { status, category } = req.payload;
-
-  const getData = await databases.listDocuments(
-    envConfig.appWriteDBId,
-    envConfig.appWriteCollectionId
-  );
-
-  console.log(status, category);
-  return res.json(getData.documents);
+  return res.json("heeloo world!!!!");
 };
