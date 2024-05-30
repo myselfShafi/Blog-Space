d70d03e5761da66ce093d796577bb8bbc52dabab
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-05-30 21:13:14 +0530

    add functions

---
+++ functions/manageArticleCount.js
@@ -0,0 +1,11 @@
+import { Client, Databases } from "node-appwrite";
+import { envConfig } from "../src/config";
+
+export default async ({ req, res, log, error }) => {
+  const client = new Client();
+  const databases = new Databases(client);
+
+  client
+    .setEndpoint(envConfig.appWriteURL)
+    .setProject(envConfig.appWriteProjectId);
+};
