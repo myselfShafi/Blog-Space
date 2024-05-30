7c58c0d54e505953903f07367a094b5cd813604d
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-05-30 23:24:31 +0530

    add env var

--- functions/manageArticleCount.js
+++ functions/manageArticleCount.js
@@ -1,38 +1,33 @@
 import { Client, Databases } from "node-appwrite";
 
-const envConfig = {
-  appWriteURL: String(import.meta.env.VITE_APPWRITE_URL),
-  appWriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-  appWriteDBId: String(import.meta.env.VITE_APPWRITE_DB_ID),
-  appWriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-  appWriteUserCollectionId: String(
-    import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID
-  ),
-  appWriteCategoryCollectionId: String(
-    import.meta.env.VITE_APPWRITE_CATEGORY_COLLECTION_ID
-  ),
-  appWriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-  appWriteUserBucketId: String(import.meta.env.VITE_APPWRITE_USER_BUCKET_ID),
-};
+const appWriteURL = process.env.APPWRITE_URL;
+const appWriteProjectId = process.env.APPWRITE_PROJECT_ID;
+const appWriteDBId = process.env.APPWRITE_DB_ID;
+const appWriteCollectionId = process.env.APPWRITE_COLLECTION_ID;
+const appWriteUserCollectionId = process.env.APPWRITE_USER_COLLECTION_ID;
+const appWriteCategoryCollectionId =
+  process.env.APPWRITE_CATEGORY_COLLECTION_ID;
+const appWriteBucketId = process.env.APPWRITE_BUCKET_ID;
+const appWriteUserBucketId = process.env.APPWRITE_USER_BUCKET_ID;
 
-export default async ({ req, res, log, error }) => {
+export default async ({ req, res }) => {
   const client = new Client();
   const databases = new Databases(client);
 
-  client
-    .setEndpoint(envConfig.appWriteURL)
-    .setProject(envConfig.appWriteProjectId);
+  client.setEndpoint(appWriteURL).setProject(appWriteProjectId);
 
-  if (req.method == "GET") {
-    const getData = await databases.listDocuments(
-      envConfig.appWriteDBId,
-      envConfig.appWriteCollectionId
-    );
+  try {
     const { status, category } = req.payload;
 
-    log(status, category);
-    console.log(status, category);
+    const getData = await databases.listDocuments(
+      appWriteDBId,
+      appWriteCollectionId
+    );
 
+    console.log({ status, category });
     return res.json(getData.documents);
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return res.status(500).json({ error: "Internal server error..." });
   }
 };
