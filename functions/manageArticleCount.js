03f1c405f85613a9d4821ca39bf462d212ff0f32
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-05-31 17:11:32 +0530

    test count function

--- functions/manageArticleCount.js
+++ functions/manageArticleCount.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from "node-appwrite";
+import { Client, Databases, ID, Query } from "node-appwrite";
 
 const appWriteURL = process.env.APPWRITE_URL;
 const appWriteProjectId = process.env.APPWRITE_PROJECT_ID;
@@ -17,12 +17,63 @@ export default async ({ req, res }) => {
   client.setEndpoint(appWriteURL).setProject(appWriteProjectId);
 
   try {
-    const getData = await databases.listDocuments(
+    const event = req.headers["x-appwrite-event"];
+    const { status, category, thumbnail } = req.body;
+    const currCategory = await databases.listDocuments(
       appWriteDBId,
-      appWriteCollectionId
+      appWriteCategoryCollectionId,
+      [Query.contains("categoryName", category)]
     );
+    let currDoc;
+    if (currCategory.total > 0 && currCategory.documents.length > 0) {
+      currDoc = currCategory.documents[0];
+    }
+    if (currCategory.total === 0 && currCategory.documents.length === 0) {
+      if (status === "public") {
+        currDoc = await databases.createDocument(
+          appWriteDBId,
+          appWriteCategoryCollectionId,
+          ID.unique(),
+          {
+            categoryName: category,
+            defaultImage: thumbnail,
+            count: 0,
+            counter: 0,
+          }
+        );
+      }
+    }
+    let newCount = currDoc.counter ?? 0;
+    if (
+      event ===
+      `databases.${appWriteDBId}.collections.${appWriteCollectionId}.documents.*.create`
+    ) {
+      if (status === "public") {
+        newCount += 1;
+      }
+    } else if (
+      event ===
+      `databases.${appWriteDBId}.collections.${appWriteCollectionId}.documents.*.delete`
+    ) {
+      if (status === "public") {
+        newCount -= 1;
+      }
+    } else if (
+      event ===
+      `databases.${appWriteDBId}.collections.${appWriteCollectionId}.documents.*.update`
+    ) {
+    }
 
-    return res.json(getData.documents);
+    if (currDoc) {
+      await databases.updateDocument(
+        appWriteDBId,
+        appWriteCategoryCollectionId,
+        currDoc.$id,
+        { counter: newCount }
+      );
+    }
+
+    res.send({ success: true });
   } catch (error) {
     console.error("An error occurred:", error);
     return res.status(500).json({ error: "Internal server error..." });
